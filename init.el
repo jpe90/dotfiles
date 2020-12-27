0d6d08740cc00c0f3b758f49f17c6166993619c5
jpe90 <patriot.game@gmail.com>
2020-12-27 10:53:43 -0500

    add initial emacs config

---
+++ init.el
@@ -0,0 +1,24 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+
+
+;; ############################################################ DO NOT MODIFY
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(naysayer))
+ '(custom-safe-themes
+   '("5d59bd44c5a875566348fa44ee01c98c1d72369dc531c1c5458b0864841f887c" default))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages '(naysayer-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
