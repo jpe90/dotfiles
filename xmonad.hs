bdc6676b101b202d31be3f3290b29e00cd84ca15
jpe <patriot.game@gmail.com>
2021-05-30 02:01:37 -0400

    update xmonad config

--- xmonad.hs
+++ xmonad.hs
@@ -1,8 +1,11 @@
 import XMonad
+import qualified Data.Map        as M
+import XMonad.Util.EZConfig ( additionalKeys )
 
 main = xmonad $ def
     { borderWidth           = 2
-    , modMask               = mod4Mask 
+    , modMask               = mod4Mask
     , terminal              = "st"
     , normalBorderColor     = "#cccccc"
-    , focusedBorderColor    = "cd8b00" }
+    , focusedBorderColor    = "cd8b00"
+	} `additionalKeys` [ ((mod4Mask .|. shiftMask, xK_q     ), kill)]
