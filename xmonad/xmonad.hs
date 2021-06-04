10e306e6a06795132ff10fa42ccd6a1921f3494c
jpe90 <patriot.game@gmail.com>
2021-06-04 14:52:41 -0400

    tmux and new nvim

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -5,7 +5,7 @@ import XMonad.Util.EZConfig ( additionalKeys )
 main = xmonad $ def
     { borderWidth           = 2
     , modMask               = mod4Mask
-    , terminal              = "st"
+    , terminal              = "kitty"
     , normalBorderColor     = "#cccccc"
     , focusedBorderColor    = "#d08770"
     } `additionalKeys` [ 
