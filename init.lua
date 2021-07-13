33775ded2691abf310067799a2cf65d89e6670f0
jpe90 <patriot.game@gmail.com>
2021-07-13 15:55:20 -0400

    the saga continues- compe commented out

--- init.lua
+++ init.lua
@@ -81,43 +81,36 @@ require('packer').startup(function()
   use 'kyazdani42/nvim-tree.lua'
   use 'elixir-editors/vim-elixir'
   use 'tomasiser/vim-code-dark'
+  use 'windwp/nvim-autopairs'
   -- use 'windwp/nvim-autopairs'
-  use {
-      "cohama/lexima.vim",
-      config = function()
-          vim.g.lexima_no_default_rules = true
-          vim.fn["lexima#set_default_rules"]()
-          vim.g.completion_confirm_key = ""
-      end
-  }
-  use {
-      "hrsh7th/nvim-compe",
-      requires = {
-          {"andersevenrud/compe-tmux"}
-      },
-      config = function()
-        require'compe'.setup {
-          enabled = true;
-          autocomplete = true;
-          debug = false;
-          min_length = 1;
-          preselect = 'enable';
-          throttle_time = 80;
-          source_timeout = 200;
-          incomplete_delay = 400;
-          max_abbr_width = 100;
-          max_kind_width = 100;
-          max_menu_width = 100;
-          documentation = true;
+  -- use {
+  --     "hrsh7th/nvim-compe",
+  --     -- requires = {
+  --     --     {"andersevenrud/compe-tmux"}
+  --     -- },
+  --     config = function()
+  --       require'compe'.setup {
+  --         enabled = true;
+  --         autocomplete = true;
+  --         debug = false;
+  --         min_length = 1;
+  --         preselect = 'enable';
+  --         throttle_time = 80;
+  --         source_timeout = 200;
+  --         incomplete_delay = 400;
+  --         max_abbr_width = 100;
+  --         max_kind_width = 100;
+  --         max_menu_width = 100;
+  --         documentation = true;
 
-          source = {
-            path = true;
-            nvim_lsp = true;
-            vsnip = true;
-          };
-        }
-      end
-    }
+  --         source = {
+  --           path = true;
+  --           nvim_lsp = true;
+  --           vsnip = true;
+  --         };
+  --       }
+  --     end
+  --   }
 end)
 use 'LnL7/vim-nix'
 vim.cmd [[
@@ -329,56 +322,49 @@ local t = function(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
 
+-- compe stuff
+-- map("i" , "<C-e>"      , "compe#confirm()" , { noremap = true , expr = true , silent = true })
+-- map("i" , "<C-l>"     , "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'" , { noremap = false , expr = true })  -- Ctrl-L to jump on placeholders.
+-- map("s" , "<C-l>"     , "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'" , { noremap = false , expr = true })
+-- map("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+-- map("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+-- map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+-- map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
-map("i" , "<C-e>"      , "compe#confirm()" , { noremap = true , expr = true , silent = true })
-map("i" , "<C-l>"     , "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'" , { noremap = false , expr = true })  -- Ctrl-L to jump on placeholders.
-map("s" , "<C-l>"     , "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'" , { noremap = false , expr = true })
-map("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-map("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 -- map("i", "<C-e>", "compe#complete()", {expr = true})
 -- map("i", "<CR>", "compe#confirm(lexima#expand('<LT>CR>', 'i'))", {expr = true})
 -- map("i", "<C-shift-e>", "compe#close('<C-e>')", {expr = true})
 
 
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif vim.fn.call("vsnip#available", {1}) == 1 then
-    return t "<Plug>(vsnip-expand-or-jump)"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return vim.fn['compe#complete']()
-  end
-end
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-    return t "<Plug>(vsnip-jump-prev)"
-  else
-    return t "<S-Tab>"
-  end
-end
+-- local check_back_space = function()
+--     local col = vim.fn.col('.') - 1
+--     if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+--         return true
+--     else
+--         return false
+--     end
+-- end
+-- _G.tab_complete = function()
+--   if vim.fn.pumvisible() == 1 then
+--     return t "<C-n>"
+--   elseif vim.fn.call("vsnip#available", {1}) == 1 then
+--     return t "<Plug>(vsnip-expand-or-jump)"
+--   elseif check_back_space() then
+--     return t "<Tab>"
+--   else
+--     return vim.fn['compe#complete']()
+--   end
+-- end
+-- _G.s_tab_complete = function()
+--   if vim.fn.pumvisible() == 1 then
+--     return t "<C-p>"
+--   elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
+--     return t "<Plug>(vsnip-jump-prev)"
+--   else
+--     return t "<S-Tab>"
+--   end
+-- end
 
 
 
