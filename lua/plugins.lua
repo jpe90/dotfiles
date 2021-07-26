8fced5cd8bff46c5ab8da9a5c099d246aff20fac
jpe <patriot.game@gmail.com>
2021-07-26 01:17:50 -0400

    cleanup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,11 +22,9 @@ require('packer').startup(function()
   use 'tpope/vim-rhubarb'            -- Fugitive-companion to interact with github
   use 'tpope/vim-commentary'         -- "gc" to comment visual regions/lines
   use 'tpope/vim-surround'         -- "gc" to comment visual regions/lines
-  use 'tpope/vim-eunuch'
   use 'tpope/vim-unimpaired'
   use 'tpope/vim-repeat'
   use 'justinmk/vim-sneak'
-  -- use 'ggandor/lightspeed.nvim'
   use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
     require('telescope').setup {
       defaults = {
@@ -36,19 +34,8 @@ require('packer').startup(function()
     }
   }
   use { 'lukas-reineke/indent-blankline.nvim', branch="master" }
-  use 'neovimhaskell/haskell-vim'
-  use 'ziglang/zig.vim'
-  use 'dart-lang/dart-vim-plugin'
   use {"npxbr/gruvbox.nvim", requires = {"rktjmp/lush.nvim"}}
-  use 'dag/vim-fish'
-  use 'rust-lang/rust.vim'
-  use "akinsho/nvim-toggleterm.lua"
-  use 'folke/tokyonight.nvim'
-  use 'Th3Whit3Wolf/one-nvim'
-  use 'axvr/photon.vim'
-  -- use 'jpe90/monokai.nvim'
-  -- use 'tanvirtin/monokai.nvim'
-  use 'sainnhe/sonokai'
+  -- use "akinsho/nvim-toggleterm.lua"
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' ,
     require'nvim-treesitter.configs'.setup {
       ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
@@ -69,12 +56,9 @@ require('packer').startup(function()
     end
   }
   use {"folke/lua-dev.nvim", opt = true}
-  use 'kyazdani42/nvim-web-devicons'
-  use 'kyazdani42/nvim-tree.lua'
-  use 'elixir-editors/vim-elixir'
+  -- use 'kyazdani42/nvim-web-devicons'
+  -- use 'kyazdani42/nvim-tree.lua'
   use 'windwp/nvim-autopairs'
-  use 'LnL7/vim-nix'
-  use 'mg979/vim-visual-multi'
   use {
     'hrsh7th/nvim-compe',
     requires = {
@@ -87,81 +71,22 @@ require('packer').startup(function()
 
 end)
 
-require('gitsigns').setup {
-  signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-  },
-  numhl = false,
-  linehl = false,
-  keymaps = {
-    -- Default keymap options
-    noremap = true,
+require('gitsigns').setup {}
 
-    ['n ]c'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
-    ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
-
-    ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ['v <leader>hS'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-    ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-
-    -- Text objects
-    ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-    ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
-  },
-  watch_index = {
-    interval = 1000,
-    follow_files = true
-  },
-  current_line_blame = false,
-  current_line_blame_delay = 1000,
-  current_line_blame_position = 'eol',
-  sign_priority = 6,
-  update_debounce = 100,
-  status_formatter = nil, -- Use default
-  word_diff = false,
-  use_decoration_api = true,
-  use_internal_diff = true,  -- If luajit is present
-}
-
-require("toggleterm").setup{
-  open_mapping = [[<M-`>]],
-}
+-- require("toggleterm").setup{
+--   open_mapping = [[<M-`>]],
+-- }
 
 
 require('nvim-autopairs').setup()
-vim.g.sonokai_style = 'espresso'
-
 
 -- tree
-vim.g.nvim_tree_auto_close = 1
-vim.g.nvim_tree_hijack_netrw = 1
-vim.api.nvim_set_keymap('n', '<C-b>', '<cmd>NvimTreeToggle<cr>', { noremap = true, silent=true})
+-- vim.g.nvim_tree_auto_close = 1
+-- vim.g.nvim_tree_hijack_netrw = 1
+-- vim.api.nvim_set_keymap('n', '<C-b>', '<cmd>NvimTreeToggle<cr>', { noremap = true, silent=true})
 
 --colors
--- require('monokai')
-vim.cmd('colorscheme sonokai')
--- vim.o.background = "dark" -- or "light" for light mode
--- vim.g.sonokai_enable_italic=1
-
-
-
--- haskell
-
-vim.g.haskell_enable_quantification=1
-vim.g.haskell_enable_recursivedo=1
-vim.g.haskell_enable_arrowsyntax=1
-vim.g.haskell_enable_pattern_synonyms=1
-vim.g.haskell_enable_typeroles=1
-vim.g.haskell_enable_static_pointers=1
+vim.cmd('colorscheme gruvbox')
 
 --Map blankline
 
