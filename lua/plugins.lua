7331403daef51cdd14e7ca7fa28b51487d6dd158
jpe90 <patriot.game@gmail.com>
2021-08-24 13:01:41 -0400

    update plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -83,11 +83,28 @@ require('packer').startup(function()
   use 'hrsh7th/cmp-buffer'
   use 'saadparwaiz1/cmp_luasnip'
 
+    --[[ config = function()
+      local actions = require "fzf-lua.actions"
+      require'fzf-lua'.setup {
+        previewers = {
+          bat = {
+          cmd             = "bat",
+          args            = "--style=numbers,changes --color always",
+          theme           = 'gruvbox-dark', -- bat preview theme (bat --list-themes)
+          config          = nil,            -- nil uses $BAT_CONFIG_PATH
+		},
+	
+        },
+
+      } ]]
+    -- end
   use { 'ibhagwan/fzf-lua',
+    local actions = require "fzf-lua.actions",
     requires = {
       'vijaymarupudi/nvim-fzf',
       'kyazdani42/nvim-web-devicons' } -- optional for icons
   }
+  
 
 
   use {
@@ -166,7 +183,7 @@ vim.cmd('colorscheme gruvbox')
 -- vim.g.neon_transparent = true
 -- vim.cmd[[colorscheme neon]]
 
-vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
+-- vim.cmd([[hi Normal guibg=NONE ctermbg=NONE]])
 
 -- require("github-theme").setup({
 --   hideInactiveStatusline = true,
