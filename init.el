a202832d8623008e71b4f9a2fa013604ea0544fa
Jon Eskin <eskinjp@gmail.com>
2023-09-29 19:13:09 -0400

    cleanup

--- init.el
+++ init.el
@@ -1,50 +1,36 @@
+;; === Packages and Initialization ===
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(use-package better-defaults
-  :ensure t)
+(require 'recentf)
+(require 'uniquify)
+(require 'view)
 
 (use-package rg
   :ensure t)
-  
-(use-package sly
-  :disabled t
-  :ensure t
-  :init
-  (setq sly-net-coding-system 'utf-8-unix)
-  (setq sly-lisp-implementations
-        '((sbcl ("sbcl") :coding-system utf-8-unix)
-          (ecl ("ecl") :coding-system utf-8-unix))))
 
 (use-package xref
   :init
-  ;; Use faster search tool
   (setq xref-search-program (cond
                              ((executable-find "ugrep") 'ugrep)
                              ((executable-find "rg") 'ripgrep)
-                             (t 'grep)))
-
-  ;; Select from xref candidates in minibuffer
-  ;(setq xref-show-definitions-function #'xref-show-definitions-completing-read
-  ;xref-show-xrefs-function #'xref-show-definitions-completing-read)
-  )
+                             (t 'grep))))
 
 (use-package magit
   :ensure t)
 
-(defun previous-buffer ()
-  "Switch to previously open buffer.
-Repeated  toggle between the two most recently open buffers."
-  (interactive)
-  (switch-to-buffer (other-buffer (current-buffer) 1)))
+(use-package smex
+  :ensure t
+  :demand
+  :config
+  (global-set-key (kbd "M-x") 'smex)
+  (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+  (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
 
 (defun meow-setup ()
   (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
-  (meow-motion-overwrite-define-key
-   '("j" . meow-next)
-   '("k" . meow-prev)
-   '("<escape>" . ignore))
   (meow-leader-define-key
    ;; SPC j/k will run the original command in MOTION state.
    '("j" . "H-j")
@@ -66,12 +52,13 @@ Repeated  toggle between the two most recently open buffers."
    '("f" . find-file)
    '("i" . imenu)
    '("a" . execute-extended-command)
-   '("=" . smerge-basic-map)
+   '("=" . org-store-link)
    '("p" . project-find-file)
    '("j" . project-switch-to-buffer)
    '("t" . tab-bar-switch-to-tab)
    '("l" . project-switch-project)	
    '("y" . magit)
+   '("l" . recentf-open-files)
    '("n" . org-roam-keymap)
    '(":" . eval-expression)
    '("e" . "C-x C-e")
@@ -153,77 +140,116 @@ Repeated  toggle between the two most recently open buffers."
    '("%" . meow-query-replace)
    '("]" . scroll-up-command)
    '("[" . scroll-down-command)
+   '("{" . backward-paragraph)
+   '("}" . forward-paragraph)
    '("<escape>" . ignore)))
-
-(use-package smex
-  :demand
-  :config
-  (global-set-key (kbd "M-x") 'smex)
-  (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-  (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command))
 
 (use-package meow
   :ensure t
   :config
   (meow-setup)
-  (meow-global-mode 1))
-
+  (meow-global-mode 1)
+  (setq meow-use-clipboard t)
+  (setq meow-visit-sanitize-completion nil)
+  )  
+  
 (use-package xclip
-  :ensure t)
-
-(use-package kaolin-themes
   :ensure t)
 
 (use-package vundo
   :ensure t)
 
-(setq
- mac-option-modifier 'meta
- set-mark-command-repeat-pop t     ;; cycle thru marks w/ c-space
- mouse-wheel-progressive-speed nil ;; turn off mouse acceleration
- read-file-name-completion-ignore-case t ;;; case insenstive autocompletion
- dired-kill-when-opening-new-dired-buffer t ;; stop dired from cluttering buffer list
- c-default-style "k&r"
- create-lockfiles nil
- minibuffer-prompt-properties
- '(read-only t cursor-intangible t face minibuffer-prompt)
- ;; indent with spaces
- c-basic-offset 4
- dired-deletion-confirmer '(lambda (x) t)
- split-height-threshold nil ;; only open horizontal splits (works on searches)
- undo-limit 20000000
- undo-strong-limit 40000000
- tags-revert-without-query t
- edebug-print-length nil
- edebug-print-level nil
- use-short-answers t
- mac-pass-command-to-system nil
- mac-pass-control-to-system nil
- )
-;; enable modes
+(use-package kaolin-themes
+  :ensure t
+  :config
+  (load-theme 'kaolin-dark t))
+
+(autoload 'zap-up-to-char "misc"
+  "Kill up to, but not including ARGth occurrence of CHAR." t)
+
+;; === Settings and Modes ===
+
+;; Various Settings
+(setq mac-option-modifier 'meta
+      set-mark-command-repeat-pop t
+      mouse-wheel-progressive-speed nil
+      read-file-name-completion-ignore-case t
+      dired-kill-when-opening-new-dired-buffer t
+      c-default-style "k&r"
+      create-lockfiles nil
+      minibuffer-prompt-properties '(read-only t cursor-intangible t face minibuffer-prompt)
+      c-basic-offset 4
+      dired-deletion-confirmer '(lambda (x) t)
+      split-height-threshold nil
+      undo-limit 20000000
+      undo-strong-limit 40000000
+      tags-revert-without-query t
+      edebug-print-length nil
+      edebug-print-level nil
+      use-short-answers t
+      mac-pass-command-to-system nil
+      mac-pass-control-to-system nil
+      recentf-max-menu-items 500
+      recentf-max-saved-items 500
+      save-interprogram-paste-before-kill t
+      apropos-do-all t
+      mouse-yank-at-point t
+      require-final-newline t
+      visible-bell t
+      load-prefer-newer t
+      backup-by-copying t
+      frame-inhibit-implied-resize t
+      ediff-window-setup-function 'ediff-setup-windows-plain
+      custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+(unless backup-directory-alist
+  (setq backup-directory-alist `(("." . ,(concat user-emacs-directory
+                                                 "backups")))))
+
+;; Modes
 (delete-selection-mode 1)
-;;(pixel-scroll-mode 1)
 (xclip-mode 1)
 (xterm-mouse-mode 1)
+(recentf-mode 1)
+(global-auto-revert-mode 1)
+(ido-everywhere)
+(show-paren-mode 1)
+(savehist-mode 1)
+(setq-default indent-tabs-mode nil)
+(save-place-mode 1)
 
-(define-key global-map (kbd "C-z") nil)
+;; === Functions ===
 
-(define-key global-map (kbd "C-<next>") nil)
-(define-key global-map "\eo" #'previous-buffer)
-(define-key global-map (kbd "C-M-o") #'delete-other-windows)
-(define-key global-map (kbd "C-c b") #'switch-to-buffer)
-;(define-key global-map (kbd "C-;") #'comment-line)
-;(define-key global-map [f2] nil)
-;(define-key global-map (kbd "C-,") 'project-find-regexp)
-;(define-key global-map (kbd "C-\\") 'project-switch-to-buffer)
-;;(define-key global-map (kbd "C-x b") 'switch-to-buffer)
-;; (define-key global-map "\e3" 'exchange-point-and-mark)
-(define-key global-map "\e`" #'other-frame)
-(define-key global-map (kbd "<C-left>") #'back-to-indentation)
-(define-key global-map (kbd "<C-right>") #'move-end-of-line)
-(define-key global-map "\en" 'forward-paragraph)
-(define-key global-map "\ep" 'backward-paragraph)
-(define-key global-map [S-tab] 'indent-for-tab-command)
-(define-key global-map (kbd "C-c p") 'project-find-file)
-(setq-default indent-tabs-mode t)
+(defun previous-buffer ()
+  "Switch to previously open buffer.
+Repeated  toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
+
+;; === Keybindings ===
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "M-z") 'zap-up-to-char)
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
+(global-set-key (kbd "C-z") nil)
+(global-set-key (kbd "C-<next>") nil)
+(global-set-key (kbd "C-M-o") 'delete-other-windows)
+(global-set-key (kbd "C-c b") 'switch-to-buffer)
+(global-set-key (kbd "<C-left>") 'back-to-indentation)
+(global-set-key (kbd "<C-right>") 'move-end-of-line)
+(global-set-key (kbd "C-c p") 'project-find-file)
+(global-set-key (kbd "M-o") 'previous-buffer)
+(global-set-key (kbd "M-n") 'forward-paragraph)
+(global-set-key (kbd "M-p") 'backward-paragraph)
+(global-set-key (kbd "M-`") 'other-frame)
+(global-set-key [S-tab] 'indent-for-tab-command)
+(define-key Info-mode-map [remap scroll-up-command] 'View-scroll-half-page-forward)
+(define-key Info-mode-map [remap scroll-down-command] 'View-scroll-half-page-backward)
+
+;; Load Custom File
 (load "~/.emacs.d/custom.el")
+
